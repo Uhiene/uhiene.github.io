0216ac78bc051d4d5db45f770d687020f0154001
uhiene <princessuhiene@gmail.com>
2024-06-15 15:22:46 +0100

    i corrected the header

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <main className="flex-grow overflow-auto">{children}</main>
+            <main className="flex-grow overflow-auto mt-12">{children}</main>
             <Footer/>
         </div>
     );
